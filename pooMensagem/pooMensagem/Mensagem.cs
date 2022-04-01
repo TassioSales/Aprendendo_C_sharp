72534b8f86f369310788e4e83f313544a764aeb1
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-04-01 12:28:12 -0300

    add arquivos

---
+++ pooMensagem/pooMensagem/Mensagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    internal class Mensagem
+    {
+        public String TextoMensagem;
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+
+      
+        }
+    }
+}
